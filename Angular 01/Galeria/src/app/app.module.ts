b5233b9bfa26a8f0a8d38d65b425fb9d055fae94
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2019-04-10 23:07:29 -0500

    actualizado rama desarrollo

--- Angular 01/Galeria/src/app/app.module.ts
+++ Angular 01/Galeria/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GaleriaComponent } from './galeria/galeria.component';
 import { ItemGaleriaComponent } from './item-galeria/item-galeria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GaleriaComponent,
     ItemGaleriaComponent
   ],
   imports: [
--- Angular 01/Galeria/src/app/item-galeria/item-galeria.component.ts
+++ Angular 01/Galeria/src/app/item-galeria/item-galeria.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-item-galeria',
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemGaleriaComponent implements OnInit {
 
+  title  = 'Licoreria'
+@Input()
+nombreItem ;
+@Input()
+textoBoton;
   constructor() { }
 
   ngOnInit() {
   }
+
+  alertar(){
+
+    alert('Auxilio mijo: '+this.nombreItem);
+
+  }
+
+}
+
+
+
+
+
+class Usuario{
+
+
+@Input()
+
+  private nombre='Michael'
+
+
+  constructor(nombre){
+
+
+}
+metodoPublico(){
+
+
+
+}
+private metodoPrivado(){
+
+
+
+}
+protected metodoProtected(){
+
+
+
+}
+
 
 }
